4305c2d80166b32f5c0d248a771471335d5bd1a7
CooledCoffee <CooledCoffee@gmail.com>
2014-03-17 14:14:43 +0800

    setup.py

---
+++ src/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='SQLAlchemy-Dao',
+    version='1.0',
+    author='Mengchen LEE',
+    author_email='CooledCoffee@gmail.com',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ],
+    description='Simple wrapper for sqlalchemy.',
+    install_requires=[
+        'decorated',
+        'fixtures',
+        'sqlalchemy',
+    ],
+    packages=[
+        'sqlalchemy_dao',
+    ],
+    url='https://github.com/CooledCoffee/sqlalchemy-dao/',
+)
