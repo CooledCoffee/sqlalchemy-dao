51f6c61b1ecb4b9fc8904ad3e75c77ad343cfc09
CooledCoffee <CooledCoffee@gmail.com>
2014-03-18 10:28:57 +0800

    remove python3 support from setup.py

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries',
     ],
     description='Simple wrapper for sqlalchemy.',
