35409f877afc0a85a1c86cc5d4dacba24afc53d0
CooledCoffee <CooledCoffee@gmail.com>
2014-03-18 11:02:51 +0800

    pool size for dao

---
+++ src/sqlalchemy_dao/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+POOL_DISABLED = -1
+POOL_NO_LIMIT = 0
+POOL_DEFAULT = 5
--- src/sqlalchemy_dao/dao.py
+++ src/sqlalchemy_dao/dao.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from sqlalchemy import engine
 from sqlalchemy.orm import session
+from sqlalchemy.pool import NullPool
 from sqlalchemy_dao.context import SessionContext
 from sqlalchemy_dao.lock import Lock
 from sqlalchemy_dao.session import Session
+import sqlalchemy_dao
 
 class Dao(object):
-    def __init__(self, url):
-        self._engine = engine.create_engine(url, echo=False)
+    def __init__(self, url, pool_size=sqlalchemy_dao.POOL_DEFAULT):
+        pool_class = NullPool if pool_size == sqlalchemy_dao.POOL_DISABLED else None
+        self._engine = engine.create_engine(url, poolclass=pool_class, pool_size=pool_size)
         self._Session = session.sessionmaker(bind=self._engine, class_=Session)
         
     def create_session(self):
