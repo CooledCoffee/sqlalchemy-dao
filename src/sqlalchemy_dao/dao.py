eb27a56cf35da8f7d5d7d2ea17d75a9ecd474b93
CooledCoffee <CooledCoffee@gmail.com>
2014-03-18 12:27:47 +0800

    max_overflow & pool_recycle for dao

--- src/sqlalchemy_dao/dao.py
+++ src/sqlalchemy_dao/dao.py
@@ -6,11 +6,13 @@ from sqlalchemy_dao.context import SessionContext
 from sqlalchemy_dao.lock import Lock
 from sqlalchemy_dao.session import Session
 import sqlalchemy_dao
+import sys
 
 class Dao(object):
     def __init__(self, url, pool_size=sqlalchemy_dao.POOL_DEFAULT):
         pool_class = NullPool if pool_size == sqlalchemy_dao.POOL_DISABLED else None
-        self._engine = engine.create_engine(url, poolclass=pool_class, pool_size=pool_size)
+        self._engine = engine.create_engine(url, poolclass=pool_class, pool_size=pool_size,
+                max_overflow=sys.maxsize, pool_recycle=3600)
         self._Session = session.sessionmaker(bind=self._engine, class_=Session)
         
     def create_session(self):
