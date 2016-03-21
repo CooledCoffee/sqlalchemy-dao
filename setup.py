e61b11bef353df6e0ea0fc719ec817e78a26a554
CooledCoffee <CooledCoffee@gmail.com>
2016-03-21 13:56:28 +0800

    version 1.2.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='SQLAlchemy-Dao',
-    version='1.2.4',
+    version='1.2.5',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [
